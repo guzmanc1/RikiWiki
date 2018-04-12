1955d52a9a39a0c5b02be287ab1892f873524c56
BrentBarnhorst <barnhorstb1@nku.edu>
2018-04-12 11:48:17 -0400

    changes to config.py and core

--- wiki_flask/config.py
+++ wiki_flask/config.py
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
 SECRET_KEY='a unique and long key'
-TITLE='Riki' 
+TITLE='RikiWiki'
 HISTORY_SHOW_MAX=30
 PIC_BASE = '/static/content/'
-CONTENT_DIR = '/Users/smcho/PycharmProjects/Riki/content'
+CONTENT_DIR = 'content'
 USER_DIR = 'user'
 NUMBER_OF_HISTORY = 5
 PRIVATE = True
